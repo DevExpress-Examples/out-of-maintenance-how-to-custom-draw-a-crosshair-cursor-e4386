ae35deb03019f5e0ea306ec94968a999b9bfaefc
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CrosshairCursor_CustomDraw/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Charts.Native;
+using DevExpress.Xpf.Charts;
+
+namespace CrosshairCursor_CustomDraw {
+    public partial class MainPage : UserControl {
+        bool handleCustomDraw;
+
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            handleCustomDraw = true;
+            chartControl1.CustomDrawCrosshair += chartControl1_CustomDrawCrosshair;
+        }
+        private void chartControl1_CustomDrawCrosshair(object sender, CustomDrawCrosshairEventArgs e) {
+            if (!handleCustomDraw) return;
+
+            // Specify the crosshair argument line color and thickness.
+            e.CrosshairLineElement.Brush = Brushes.Red;
+            e.CrosshairLineElement.LineStyle.Thickness = 3;
+
+            // Specify  the background  and font size for the crosshair argument label. 
+            foreach (CrosshairAxisLabelElement axisLabelElement in e.CrosshairAxisLabelElements) {
+                axisLabelElement.Background = Brushes.Blue;
+                axisLabelElement.FontSize = 14;
+            }
+
+            foreach (CrosshairElement element in e.CrosshairElements) {
+                // Specify the color and thickness for the crosshair value lines.                 
+                element.LineElement.Brush = Brushes.Blue;
+                element.LineElement.LineStyle.Thickness = 2;
+
+                // Specify the  font size and background for the crosshair value labels.
+                element.AxisLabelElement.FontSize = 14;
+                element.AxisLabelElement.Background = Brushes.Red;
+
+                // Specify the foreground and  font size for the crosshair  cursor label that shows series. 
+                element.LabelElement.Foreground = Brushes.Red;
+                element.LabelElement.FontSize = 14;
+            }
+
+            foreach (CrosshairGroupHeaderElement groupHeaderElement in e.CrosshairGroupHeaderElements) {
+
+                //  Specify the font style, foreground and font size for the crosshair group header element.  
+                groupHeaderElement.FontStyle = FontStyles.Italic;
+                groupHeaderElement.Foreground = Brushes.Red;
+                groupHeaderElement.FontSize = 15;
+
+            }
+        }
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to custom draw a crosshair cursor
+
+
+<p>This example shows how to provide custom appearance of the crosshair cursor using the <a href="http://help.devexpress.com/#Silverlight/DevExpressXpfChartsChartControl_CustomDrawCrosshairtopic"><u>ChartControl.CustomDrawCrosshair</u></a> event. This event is invoked when you click the <strong>Custom Draw Crosshair Cursor </strong>button on the form. </p><p>If you wish to display crosshair axis  lines and labels before custom drawing the crosshair cursor, set the <a href="http://help.devexpress.com/#Silverlight/DevExpressXpfChartsCrosshairOptions_ShowArgumentLabelstopic"><u>CrosshairOptions.ShowArgumentLabels</u></a>, <a href="http://help.devexpress.com/#Silverlight/DevExpressXpfChartsCrosshairOptions_ShowArgumentLinetopic"><u>CrosshairOptions.ShowArgumentLine</u></a>, <a href="http://help.devexpress.com/#Silverlight/DevExpressXpfChartsCrosshairOptions_ShowValueLabelstopic"><u>CrosshairOptions.ShowValueLabels</u></a> and <a href="http://help.devexpress.com/#Silverlight/DevExpressXpfChartsCrosshairOptions_ShowValueLinetopic"><u>CrosshairOptions.ShowValueLine</u></a> properties to<strong> true</strong>. </p><p>Note that the customization of the crosshair cursor is provided for the <a href="http://help.devexpress.com/#Silverlight/DevExpressXpfChartsCrosshairOptions_SnapModetopic"><u>CrosshairOptions.SnapMode</u></a> property set to <strong>NearestArgument</strong>.</p><p><br />
+</p>
+
+<br/>
+
+
